1f5773e572ac7a2cd8b185f45877743fd88d4fc3
maxv33f1x5 <max01.f1x4@gmail.com>
2026-05-16 13:06:28 +0530

    Create BankAccount.java

---
+++ src/main/java/com/example/app/BankAccount.java
@@ -0,0 +1,36 @@
+package com.example.app;
+
+public class BankAccount {
+
+    private int accountNumber;
+    private String accountHolder;
+    private double balance;
+
+    public BankAccount(int accountNumber, String accountHolder, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void displayAccountDetails() {
+        System.out.println("\nAccount Number : " + accountNumber);
+        System.out.println("Account Holder : " + accountHolder);
+        System.out.println("Balance        : ₹" + balance);
+    }
+}
