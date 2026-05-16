3d8a8e7adbf9d40bd92f1a7c195b5916b29a9b54
maxv33f1x5 <max01.f1x4@gmail.com>
2026-05-16 13:21:07 +0530

    Update Transaction.java

--- src/main/java/com/example/app/Transaction.java
+++ src/main/java/com/example/app/Transaction.java
@@ -1,5 +1,24 @@
 package com.example.app;
 
+public class Transaction {
+
+    private String type;
+    private double amount;
+
+    public Transaction(String type, double amount) {
+        this.type = type;
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}package com.example.app;
+
 public class BankService {
 
     public static void deposit(BankAccount account, double amount) {
