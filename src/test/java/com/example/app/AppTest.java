49113bce5f1f6edf0562deaa3dd01e93cfb9a318
maxv33f1x5 <max01.f1x4@gmail.com>
2026-05-16 13:11:08 +0530

    Update AppTest.java

--- src/test/java/com/example/app/AppTest.java
+++ src/test/java/com/example/app/AppTest.java
@@ -7,29 +7,73 @@ import static org.junit.Assert.assertEquals;
 public class AppTest {
 
     @Test
-    public void testBookCreation() {
+    public void testDeposit() {
 
-        Book book = new Book(
-                101,
-                "Java Programming",
-                "James Gosling"
+        BankAccount account =
+                new BankAccount(1001, "Aryan", 5000);
+
+        BankService.deposit(account, 2000);
+
+        assertEquals(
+                7000,
+                account.getBalance(),
+                0.01
         );
-
-        assertEquals(101, book.getId());
     }
 
     @Test
-    public void testBookToString() {
+    public void testWithdraw() {
 
-        Book book = new Book(
-                102,
-                "Maven Essentials",
-                "John Doe"
+        BankAccount account =
+                new BankAccount(1001, "Aryan", 5000);
+
+        BankService.withdraw(account, 1000);
+
+        assertEquals(
+                4000,
+                account.getBalance(),
+                0.01
+        );
+    }
+
+    @Test
+    public void testTransfer() {
+
+        BankAccount sender =
+                new BankAccount(1001, "Aryan", 5000);
+
+        BankAccount receiver =
+                new BankAccount(1002, "Rahul", 3000);
+
+        BankService.transfer(sender, receiver, 1000);
+
+        assertEquals(
+                4000,
+                sender.getBalance(),
+                0.01
         );
 
-        String expected =
-                "Book ID: 102 | Name: Maven Essentials | Author: John Doe";
+        assertEquals(
+                4000,
+                receiver.getBalance(),
+                0.01
+        );
+    }
 
-        assertEquals(expected, book.toString());
+    @Test
+    public void testInterestCalculation() {
+
+        double interest =
+                InterestCalculator.calculateInterest(
+                        5000,
+                        5,
+                        2
+                );
+
+        assertEquals(
+                500,
+                interest,
+                0.01
+        );
     }
 }
