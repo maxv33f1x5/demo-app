d9ce275517980974b69c13918b0d4de7f350afbc
maxv33f1x5 <max01.f1x4@gmail.com>
2026-05-16 13:07:16 +0530

    Create BankService.java

---
+++ src/main/java/com/example/BankService.java
@@ -0,0 +1,68 @@
+package com.example.app;
+
+public class BankService {
+
+    public static void deposit(BankAccount account, double amount) {
+
+        account.setBalance(account.getBalance() + amount);
+
+        System.out.println("\n₹" + amount + " Deposited Successfully");
+        System.out.println("Updated Balance : ₹" + account.getBalance());
+
+        TransactionService.addTransaction(
+                "DEPOSIT",
+                amount
+        );
+    }
+
+    public static void withdraw(BankAccount account, double amount) {
+
+        if (account.getBalance() >= amount) {
+
+            account.setBalance(account.getBalance() - amount);
+
+            System.out.println("\n₹" + amount + " Withdrawn Successfully");
+            System.out.println("Updated Balance : ₹" + account.getBalance());
+
+            TransactionService.addTransaction(
+                    "WITHDRAW",
+                    amount
+            );
+
+        } else {
+
+            System.out.println("\nInsufficient Balance");
+        }
+    }
+
+    public static void transfer(
+            BankAccount sender,
+            BankAccount receiver,
+            double amount
+    ) {
+
+        if (sender.getBalance() >= amount) {
+
+            sender.setBalance(sender.getBalance() - amount);
+
+            receiver.setBalance(receiver.getBalance() + amount);
+
+            System.out.println("\n₹" + amount + " Transfer Successful");
+            System.out.println(
+                    "Transferred From Account "
+                            + sender.getAccountNumber()
+                            + " To Account "
+                            + receiver.getAccountNumber()
+            );
+
+            TransactionService.addTransaction(
+                    "TRANSFER",
+                    amount
+            );
+
+        } else {
+
+            System.out.println("\nInsufficient Balance For Transfer");
+        }
+    }
+}
